6a4695136961afecf1585e1aff6d14710e02d612
Daniel Galibov <Daniel.galibov@gmail.com>
2024-01-09 13:50:38 -0500

    Updated comments and explained workflow
    Updates the comments and explained what each portion does and some potentially confusing code

--- MapGenerator.java
+++ MapGenerator.java
@@ -2,11 +2,13 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+//mapGenerator is used to create the layout of the bricks and is just a means of organization and encapsulation
 public class MapGenerator {
-    public int map[][];
+    public int map[][]; //this is just the 2D array used to make the brick
     public int brickWidth;
     public int brickHeight;
-
+//the number of bricks is decided in the GamePlay.Java file, each "brick" in the 2D "map" array receives value 1
+    //the value 1 can be changed in the future to make the bricks smaller on the first hit, instead of just being destroyed with one hit
     public MapGenerator(int row, int col){
         map = new int[row][col];
         for (int i=0; i< map.length; i++){
@@ -33,7 +35,7 @@ public class MapGenerator {
             }
         }
     }
-
+//the SetBrickValue function is used in the GamePlay file to set the value in the 2D array of the brick to zero when the "ball" intersects with a "brick"
     public void setBrickValue(int value, int row, int col){
         map[row][col] = value;
     }
